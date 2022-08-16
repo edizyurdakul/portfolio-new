541000c0ed36ea39c3aab14bbb32546c9374d517
Ediz <53665610+edizyurdakul@users.noreply.github.com>
2022-08-17 00:19:34 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Next.js App</title>
+        <meta name="description" content="Next.js App" />
+      </Head>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
