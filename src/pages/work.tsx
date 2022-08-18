d1f381967a6f753858873a0688ec6fb0bdc86b4d
Ediz <53665610+edizyurdakul@users.noreply.github.com>
2022-08-18 17:15:23 +0200

    add placeholder content

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from "next";
+import Section from "../components/Section";
+import { motion } from "framer-motion";
+import { elements } from "../constants";
 
 const Work: NextPage = () => {
-  return <div>About</div>;
+  return (
+    <Section id="#introduction" className="py-32">
+      <motion.h1 variants={elements} className="text-4xl font-bold">
+        About Me
+      </motion.h1>
+      <motion.p variants={elements} className="pt-8 pb-4">
+        I’m Ediz, a front-end developer based in Cape Town, South Africa. I
+        spend most of my time learning more to develop my skills.
+      </motion.p>
+      <motion.p variants={elements}>Currently learning the T3 stack!</motion.p>
+    </Section>
+  );
 };
 
 export default Work;
